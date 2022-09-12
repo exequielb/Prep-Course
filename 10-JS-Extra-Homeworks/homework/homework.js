e455d0b4e31ba24160c9cd50c4f04f618e649933
exequielb <sidvicious1286@gmail.com>
2022-09-12 13:11:47 -0300

    JSX DONE

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,23 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  let clave=Object.keys(objeto)
+  let valor=Object.values(objeto)
+  let newarray=[];
+ 
+
+  for (var i = 0; i < clave.length; i++) {
+  newarray[i]=(clave[i]+"."+valor[i]).split(".")
+  }
+  for (var i = 0; i < newarray.length; i++) {
+ 
+  newarray[i][1]=Number(newarray[i][1])
+
+
+  }
+
+
+  return newarray
 }
 
 
@@ -18,6 +35,33 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  let obj={};
+  let abc="abcdefghjiklmnñopqrstuvwxyz"
+  let contador=0;
+  
+  //recorre el abecedario
+  for (var i = 0; i < abc.length; i++) {
+  //se pregunta si existe letra en abecedario
+    if(string.includes(abc[i])){
+      contador=0;
+  //recorre las letras de string y cuenta las repetidas usando acumulador
+  //si no hay coincidencia saltea la letra seleccionada y pasa a otra.
+  for (var e = 0; e < string.length; e++) {
+ 
+      if(string[e]===abc[i]){
+        contador++;
+      }
+    }
+
+    }else{
+      continue;
+    }
+  //carga los resultados/propiedades en el objeto donde clave=coincidencia y
+  //valor=nº de repeticiones
+    obj[abc[i]]=contador;  
+  }
+//devuelve el objeto
+  return obj;
 }
 
 
@@ -26,6 +70,20 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  let strings=Array.from(s);
+  let mayus=[];
+  let minus=[];  
+
+   for (var i = 0; i < strings.length; i++) {
+      if (strings[i]===strings[i].toUpperCase()){
+      mayus.push(strings[i]);
+     
+      }else{
+      minus.push(strings[i])
+      }
+     
+   }
+   return mayus.join("")+minus.join("")
 }
 
 
@@ -35,6 +93,11 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var str=str.split("").reverse().join("").split(" ").reverse().join(" ");
+
+  return str;
+
+
 } 
 
 
@@ -43,6 +106,15 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  let compare=numero+"";
+  let reverso=Array.from(numero+"").reverse().join("");
+
+  if(compare===reverso){
+  return "Es capicua";
+  }else{
+ 
+  return "No es capicua";
+  }
 }
 
 
@@ -50,6 +122,22 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+
+  let target="abc";
+  let eliminadas="";
+ 	
+
+
+  for (let i = 0; i < cadena.length; i++) {
+ 
+  if (target.includes(cadena[i])==false){
+
+  eliminadas=eliminadas+cadena[i];
+
+ 
+  }
+  }
+  return eliminadas;
 }
 
 
@@ -57,6 +145,12 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  
+   
+  arr.sort(function(a, b) {
+    return a.length - b.length;
+  });
+      return arr
 }
 
 
@@ -65,7 +159,18 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí 
+  let intersec=[];
+ 
+
+  for (var i = 0; i < arreglo1.length; i++) {
+  if(arreglo2.includes(arreglo1[i]) && intersec.includes(arreglo1[i])===false){
+  intersec.push(arreglo1[i]);
+ 
+  }
+  }
+  return intersec;
+
 }
 
 
